fdf9534f84a6e2168cdc74e03e7976822646826a
Jeffrey <123974@student.drenthecollege.nl>
2020-09-25 10:19:58 +0200

    Geuploade foto laten zien op bandpagina

--- Eindopdracht/resources/views/bandpage.blade.php
+++ Eindopdracht/resources/views/bandpage.blade.php
@@ -5,10 +5,7 @@
 <div class="container">
     <div class="row row-padding">
         <div class="col-md-6">
-            <div class="card" style="padding: 50px;height: 252px;background-image: url(https://images.pexels.com/photos/1763075/pexels-photo-1763075.jpeg?cs=srgb&dl=pexels-sebastian-ervi-1763075.jpg&fm=jpg);background-repeat: no-repeat;background-size: cover;background-position: center;">
-                {{-- <img src="https://images.pexels.com/photos/1763075/pexels-photo-1763075.jpeg?cs=srgb&dl=pexels-sebastian-ervi-1763075.jpg&fm=jpg"> --}}
-                {{-- <h2>Band foto</h2> --}}
-                {{-- <p>LOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUMLOREM IPSUM</p> --}}
+            <div class="card" style="padding: 50px;height: 252px;background-image: url(/storage/images/{{ $band->image }});background-repeat: no-repeat;background-size: cover;background-position: center;">
             </div>
         </div>
         <div class="col-md-6">
